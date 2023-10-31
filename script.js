4c57a5b14c906763bda4ac3071245becb47fda0d
Naveen-Raj-Sivasamy <naveenrajsivasamy@gmail.com>
2023-10-31 22:52:27 +0530

    Answers added

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ console.log("Question 1.a: Print odd numbers in an array")
 var myresult = [];
 
 var anonymous = function(myarrar)
+//I am accessing the anonymous function using the variable anonymous here
 {
 
     for(i=0; i<myarrar.length; i++)
@@ -21,3 +22,22 @@ var anonymous = function(myarrar)
 }
 
 console.log(anonymous([101, 102, 103, 104, 111, 112, 999]))
+
+//IIFE function
+
+
+var myresult01 = [];
+
+(function (samplearray01)
+{
+
+    for(var i=0; i<samplearray01.length; i++)
+
+    if(samplearray01[i]%2!==0)
+    {
+        myresult01.push(samplearray01[i]);
+    }
+
+console.log(myresult01);
+
+})([1, 23, 45, 46, 98, 99, 898, 5, 66, 90, 13]);
